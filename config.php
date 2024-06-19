67708bc6268640e6bb61794071c931e071426358
Gramirez23 <gpramirezecheverria@gmail.com>
2024-06-19 15:45:19 +0000

    configmap

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 	ini_set('display_errors',1);
 	error_reporting(-1);
-	define('DB_HOST', 'db');
+    define('DB_HOST', 'db');
     define('DB_USER', 'user');
-    define('DB_PASSWORD', 'user123');
+    define('DB_PASSWORD', getenv('DB_PASSWORD')); # Se obtiene de un secret
     define('DB_DATABASE', 'obligatorio');
 ?>
